347ffb682609f198e371767a8f430509bcfe6136
Aragas <micvitalij@yandex.ru>
2017-04-28 22:18:22 +0300

    Added BaseProfileFolder

---
+++ Storage/Folders/BaseProfileFolder.cs
@@ -0,0 +1,9 @@
+using PCLExt.FileStorage;
+
+namespace P3D.Legacy.Shared.Storage.Folders
+{
+    public abstract class BaseProfileFolder : BaseFolder
+    {
+        public BaseProfileFolder(IFolder folder) : base(folder) { }
+    }
+}
